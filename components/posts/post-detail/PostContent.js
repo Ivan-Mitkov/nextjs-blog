be00ef937e81bb8b03ba20022d3dbce9c2fe21ec
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-21 10:05:08 +0200

    rendering images with md files and next/Image

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,13 +1,44 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./PostHeader";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath}></PostHeader>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- content/posts/next.md
+++ content/posts/next.md
@@ -17,6 +17,8 @@ First, the page takes longer to become visible to the user, because before the c
 
 Second, if you are building a publicly available website, you have a content SEO issue. Search engines are getting better at running and indexing JavaScript apps, but it's much better if we can send them content instead of letting them figure it out.
 
+![Search Engine Optimisation](search.jpg)
+
 The solution to both of those problems is server rendering, also called static pre-rendering.
 
 Next.js is one React framework to do all of this in a very simple way, but it's not limited to this. It's advertised by its creators as a zero-configuration, single-command toolchain for React apps.
